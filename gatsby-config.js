1487d1562f3a3a2cd762b871d34cd70f1f1886a8
Luis Martin Schick <luis3720@hotmail.com>
2022-01-20 15:05:11 +0200

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Luis Martin Schick | Portfolio`,
+        short_name: `Luis Martin  Schick`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
